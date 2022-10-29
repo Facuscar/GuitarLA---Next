ca8c1fe355ac85d6dbf970717507f985150fb5e6
Facuscar <facundo.scardillo@dracoder.com>
2022-10-29 17:43:52 -0300

    Add: first page

---
+++ pages/aboutus.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+const AboutUs = () => {
+    return (
+        <Link href="/">Front page</Link>
+    )
+}
+
+export default AboutUs;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Hello, World! (In NextJS)</h1>
+
+      <Link href="/aboutus">About us</Link>
     </>
   )
 }
