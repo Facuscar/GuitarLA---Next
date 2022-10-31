757283d8c4b817f68a93d191102143c1975d5043
Facuscar <facundo.scardillo@dracoder.com>
2022-10-31 19:07:56 -0300

    Add: styles to the header

--- components/header.js
+++ components/header.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../public/img/logo.svg";
+import styles from "../styles/header.module.css"
 
 const Header = () => {
     	return (
-            <header>
+            <header className={styles.header}>
                 <div className="container">
                     <Image src={logo.src} alt='Logo of guitarLA in white and gold' width={300} height={40} />
 
-                    <nav>
+                    <nav className={styles.nav}>
                         <Link href="/">Front page</Link>      
                         <Link href="/aboutus">About us</Link>         
                         <Link href="/blog">Blog</Link>  
