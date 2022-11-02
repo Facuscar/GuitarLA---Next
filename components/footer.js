b3d7974de674ef218383baf6b1a241443a996929
Facuscar <facundo.scardillo@dracoder.com>
2022-11-01 22:55:28 -0300

    Add: first steps to the footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className="container">
+                <nav className="nav">
+                    <Link href="/" >Front page</Link>      
+                    <Link href="/aboutus" >About us</Link>         
+                    <Link href="/blog" >Blog</Link>  
+                    <Link href="/store" >Store</Link>                 
+                </nav>
+                <p>All rights reserved {new Date().getFullYear()}</p>
+            </div>
+        </footer>
     )
 }
 
