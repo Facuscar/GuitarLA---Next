10b84e47f13a5bea58b1d217c9958d6dd93f3a3d
Facuscar <facundo.scardillo@dracoder.com>
2022-11-04 01:54:42 -0300

    Add: styles

--- components/guitar-list.js
+++ components/guitar-list.js
@@ -1,12 +1,13 @@
 import Guitar from "./guitar";
+import styles from "../styles/grid.module.css"
 
 const GuitarList = ({guitars}) => {
     return (
-        <>
+        <div className={styles.grid}>
             {guitars?.map( guitar => (
                 <Guitar guitar={guitar.attributes} key={guitar.id} />
             ))}
-        </>
+        </div>
     );
 }
 
--- components/guitar.js
+++ components/guitar.js
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from "../styles/guitars.module.css"
+
 const Guitar = ({guitar}) => {
     const { description, image, name, price, url} = guitar;
     return (
-        <div>
+        <div className={styles.guitar}>
             <Image src={image.data.attributes.formats.medium.url} alt={`Guitar ${name} on a white background`} width={600} height={400} />
-            <div>
+            <div className={styles.content}>
                 <h3>{name}</h3>
-                <p>{description}</p>
-                <p>${price}</p>
-                <Link href={`/guitar/${url}`}>See guitar</Link>
+                <p className={styles.description}>{description}</p>
+                <p className={styles.price}>${price}</p>
+                <Link href={`/guitar/${url}`} className={styles.link} >See guitar</Link>
             </div>
         </div>
     );
