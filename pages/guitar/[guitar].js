eb54bbe22090799ca147c3b88b59228566d31f1b
Facuscar <facundo.scardillo@dracoder.com>
2022-11-05 23:05:32 -0300

    Add: content to guitar page

--- pages/guitar/[guitar].js
+++ pages/guitar/[guitar].js
@@ -1,3 +1,8 @@
+import Image from "next/image";
+
+import styles from "../../styles/guitars.module.css";
+import Layout from "../../components/layout";
+
 export const getServerSideProps = async ({ query: { guitar } }) => {
     console.log(guitar);
     const res = await fetch(`${process.env.API_URL}guitarras?filters[url]=${guitar}&populate=image`);
@@ -10,10 +15,21 @@ export const getServerSideProps = async ({ query: { guitar } }) => {
     };
 }
 
-const Guitar = () => {
+const Guitar = ({ guitar }) => {
+
+    const { name, description, image, price } = guitar[0].attributes;
 
     return (
-        <h1>Hi!</h1>
+        <Layout title={`GuitarLA - Guitar ${guitar}`} description={description} >
+            <div className={styles.guitar}>
+                <Image src={image.data.attributes.formats.medium.url} alt={`Guitar ${name} on a white background`} width={600} height={400} />
+                <div className={styles.content}>
+                    <h3>{name}</h3>
+                    <p className={styles.description}>{description}</p>
+                    <p className={styles.price}>${price}</p>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
