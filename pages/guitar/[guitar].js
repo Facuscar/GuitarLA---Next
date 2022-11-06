72629df9dd352c5a302528f12f1613638a3a074f
Facuscar <facundo.scardillo@dracoder.com>
2022-11-05 22:53:21 -0300

    get guitar with serverSideProps

--- pages/guitar/[guitar].js
+++ pages/guitar/[guitar].js
@@ -1,8 +1,17 @@
-import { useRouter } from "next/router";
+export const getServerSideProps = async ({ query: { guitar } }) => {
+    console.log(guitar);
+    const res = await fetch(`${process.env.API_URL}guitarras?filters[url]=${guitar}&populate=image`);
+    const data = await res.json();
+
+    return {
+        props: {
+           guitar: data.data
+        }
+    };
+}
 
 const Guitar = () => {
-    const router = useRouter();
-    
+
     return (
         <h1>Hi!</h1>
     );
