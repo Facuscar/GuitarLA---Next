0691c0f731528b1ddca48c15b827c0225d34eb6b
Facuscar <facundo.scardillo@dracoder.com>
2022-11-11 22:37:53 -0300

    Add: select in the cart

--- pages/cart.js
+++ pages/cart.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Layout from "../components/layout";
 import styles from "../styles/cart.module.css";
 
-export const Cart = ({ cart }) => {
+export const Cart = ({ cart, updateAmount }) => {
     return (
         <Layout title="GuitarLA - Shopping Cart">
             <main className="container">
@@ -13,16 +13,28 @@ export const Cart = ({ cart }) => {
                     <div className={styles.cart}>
                         <h2>Articles</h2>
 
-                        {cart.length === 0 ? 'Empty cart' : (
-                            cart.map( guitar => (
+                        {cart?.length === 0 ? 'Empty cart' : (
+                            cart?.map( guitar => (
                                 <div key={guitar.id} className={styles.guitar}>
                                     <div>
                                         <Image src={guitar.image} width={250} height={480} alt={`Guitar ${guitar.name} in a white background`} />
                                     </div>
                                     <div>
                                         <p className={styles.name}>{guitar.name}</p>
-                                        <p className={styles.price}>$<span>{guitar.price}</span></p>
-                                        <p className={styles.subtotal}>Subtotal: $<span>{guitar.price * guitar.amount}</span></p>
+
+                                        <div className={styles.cantidad}>
+                                            <p>Cantidad: </p>
+                                            <select className={styles.select} onChange={ e => updateAmount({ id: guitar.id, amount: e.target.value })} value={guitar.amount}>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                            </select>
+                                        </div>
+
+                                        <p className={styles.price}><span>${guitar.price}</span></p>
+                                        <p className={styles.subtotal}>Subtotal: <span>${guitar.price * guitar.amount}</span></p>
                                     </div>
                                 </div>
                             ))
